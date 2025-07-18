28faed0ae0e4134c56234d74404feb1e2012ba20
eduardonunespp <carlospn200406@gmail.com>
2025-07-18 11:11:30 -0300

    feat: add default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListagemClientesComponent } from './pages/listagem-clientes/listagem-clientes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListagemClientesComponent
+    }
+];
