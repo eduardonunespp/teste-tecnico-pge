9db07c27201da07a2e08e8e7c568a76e92bc4ff2
eduardonunespp <carlospn200406@gmail.com>
2025-07-19 18:32:17 -0300

    feat: add cadastro clientes route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListagemClientesComponent } from './pages/listagem-clientes/listagem-clientes.component';
+import { CadastroClientePageComponent } from './pages/cadastro-cliente-page/cadastro-cliente-page.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: ListagemClientesComponent
+    },
+    {
+        path: 'cadastro-cliente',
+        component: CadastroClientePageComponent
     }
 ];
